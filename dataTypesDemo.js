4372c3f79c4551348d84178e14d74abcebaa4224
debajyotihui1707 <debajyotihui1707@gmail.com>
2024-09-25 10:10:27 +0000

    Added data type and variable name rules

---
+++ dataTypesDemo.js
@@ -0,0 +1,12 @@
+let productPrice = 150000;
+
+let productDiscount = 15;
+
+let ProductName = "Apple Iphone 13 Pro Max (512 GB, Blue)";
+
+let isAvailable = true;
+
+console.log(productPrice);
+console.log(productDiscount);
+console.log(ProductName);
+console.log(isAvailable);
---
+++ variableName.js
@@ -0,0 +1,5 @@
+let first_name = "Debajyoti";
+let last$name = "Hui";
+
+console.log(first_name);
+console.log(last$name);
