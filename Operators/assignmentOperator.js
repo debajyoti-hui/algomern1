98b16cf5e78732b87be2225d9c86ab39e6d2ebda
debajyotihui1707 <debajyotihui1707@gmail.com>
2024-09-26 10:30:47 +0000

    logical operators added

--- Operators/assignmentOperator.js
+++ Operators/assignmentOperator.js
@@ -16,3 +16,16 @@ console.log(x);
 
 x**=2;
 console.log(x);
+
+// divides 10 by 3
+console.log(10/3);
+
+// divides 10 by 3 and rounds down to the nearest integer
+console.log(Math.floor(10/3));
+
+// divides 10 by 3 and rounds up to the nearest integer
+console.log(Math.ceil(10/3));
+
+// divides 10 by 3 and rounds to the nearest integer
+console.log(Math.round(10/3))
+
